27812a0bd28497feb6a6bc2d9feb2e65eb5112a2
Greycell <dominik.severin@yandex.com>
2022-10-18 14:34:59 +0200

    Alien Shoot added Player Life added Game Over Sequence

--- Game/MainWindow.xaml.cs
+++ Game/MainWindow.xaml.cs
@@ -21,13 +21,18 @@ namespace Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool moveLeft, moveRight, alienMove;
-        int playerSpeed = 10;
-        int alienSpeed = 5;
+        bool moveLeft, moveRight, moveUp, moveDown, alienMove, ashoot;
+        int playerSpeed = 5;
+        int alienSpeed = 7;
+        int ufoLife = 100;
+        int playerLife = 5;
+        int alienshoottick = 0;
+
+        List<Rectangle> itemstoremove = new List<Rectangle>();
         public MainWindow()
         {
             DispatcherTimer gameTime = new DispatcherTimer();
-
+            
             InitializeComponent();
 
             gameTime.Interval = TimeSpan.FromMilliseconds(20);
@@ -58,6 +63,28 @@ namespace Game
             {
                 moveRight = true;
             }
+         if(e.Key == Key.Up)
+            {
+                moveUp = true;
+            }
+         if(e.Key == Key.Down)
+            {
+                moveDown = true;
+            }
+         if(e.Key == Key.Space)
+            {
+                Rectangle bullet = new Rectangle
+                {
+                    Tag = "bullet",
+                    Height = 20,
+                    Width = 5,
+                    Fill = Brushes.Red,
+                    Stroke = Brushes.Black
+                };
+                Canvas.SetTop(bullet, Canvas.GetTop(player) - bullet.Height);
+                Canvas.SetLeft(bullet, Canvas.GetLeft(player) + player.Width / 2);
+                MyCanvas.Children.Add(bullet);
+            }
         }
 
         private void onKeyUp(object sender, KeyEventArgs e)
@@ -70,9 +97,24 @@ namespace Game
             {
                 moveRight = false;
             }
+            if(e.Key == Key.Up)
+            {
+                moveUp = false;
+            }
+            if(e.Key == Key.Down)
+            {
+                moveDown = false;
+            }
         }
         private void gameEngine(object sender, EventArgs e)
         {
+            alienshoottick++;
+            ufolifelabel.Content = "Ufo Life: " + ufoLife;
+            playerlifelabel.Content = "Player Life: " + playerLife;
+
+            Rect ralien = new Rect(Canvas.GetLeft(alien), Canvas.GetTop(alien), alien.Width, alien.Height);
+            Rect playerhitbox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+
             if(moveLeft && Canvas.GetLeft(player)>10)
             {
                 Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed);
@@ -80,13 +122,44 @@ namespace Game
             if (moveRight && Canvas.GetLeft(player) + 90 < Application.Current.MainWindow.Width)
             {
                 Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
-            }            
+            }
+            if(moveUp && Canvas.GetTop(player) > 125)
+            {
+                Canvas.SetTop(player, Canvas.GetTop(player) - playerSpeed);
+            }
+            if(moveDown && Canvas.GetTop(player) + 100 < Application.Current.MainWindow.Height)
+            {
+                Canvas.SetTop(player, Canvas.GetTop(player) + playerSpeed);
+            }
+            foreach(var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                if(x is Rectangle && (string)x.Tag == "bullet")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) - 20);
+                    if(Canvas.GetTop(x) < 10)
+                    {
+                        itemstoremove.Add(x);
+                    }
+                    Rect rbullet = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                    if(rbullet.IntersectsWith(ralien))
+                    {
+                        ufoLife -= 1;
+                        itemstoremove.Add(x);
+                        if(ufoLife == 0)
+                        {
+                            MessageBox.Show("You WIN!");
+                        }
+
+                    }
+                }
+            }
             if(alienMove == true)
             {
                 Canvas.SetLeft(alien, Canvas.GetLeft(alien) + alienSpeed);                
                 if(Canvas.GetLeft(alien) > 320)
                 {
                     alienMove = false;
+                    
                 }
             }
             if(alienMove == false)
@@ -97,6 +170,55 @@ namespace Game
                     alienMove = true;
                 }
             }
+            Rectangle alienshoot = new Rectangle
+            {
+                Tag = "alienshoot",
+                Height = 20,
+                Width = 10,
+                Fill = Brushes.LightGreen,
+                Stroke = Brushes.Black
+            };
+            Canvas.SetTop(alienshoot, Canvas.GetTop(alien) + alienshoot.Height*2);
+            Canvas.SetLeft(alienshoot, Canvas.GetLeft(alien) + alien.Width / 2);
+            if (alienshoottick == 20)
+            {
+                MyCanvas.Children.Add(alienshoot);
+                alienshoottick = 0;
+            }
+            foreach(var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                if(x is Rectangle && (string)x.Tag == "alienshoot" )
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) + 10);
+                    if(Canvas.GetTop(x) > 580)
+                    {
+                        itemstoremove.Add(x);                        
+                    }
+                    Rect ahitbox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                    if(ahitbox.IntersectsWith(playerhitbox))
+                    {
+                        playerLife -= 1;                        
+                        itemstoremove.Add(x);
+                    }
+                }
+            }
+            if (playerLife == 0)
+            {
+                MessageBox.Show("Game Over! You LOST");
+                Environment.Exit(0);
+            }
+            if (ufoLife == 0)
+            {
+                MessageBox.Show("You WIN! You saved the Planet!");
+                Environment.Exit(0);
+            }
+
+            foreach (var x in itemstoremove)
+            {
+                MyCanvas.Children.Remove(x);
+            }
+
+            
         }
     }
 }
